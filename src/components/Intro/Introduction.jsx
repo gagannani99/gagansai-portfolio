b97be7de8d6e69ee3ada36088db3e609e2217c54
Gagan Sai Alasapuri <128957984+gagannani99@users.noreply.github.com>
2024-12-08 17:03:59 +0530

    Update Introduction.jsx

--- src/components/Intro/Introduction.jsx
+++ src/components/Intro/Introduction.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
 import './intro.css'
-import {Link} from 'react-scroll'
-import bag from './pictures/bag.png'
-import me from './pictures/me.png'
+import bag from './pictures/mypic.jpeg'
 const Introduction = (props) => {
   return (
     <section id="intro">
-      <div className="introContent">
-       
+      
+      <div className="container d-flex flex-column justify-content-center">
+        <div className="row d-flex flex-row justify-content-center">
+          <div className="col-md-7 col-12 introContent order-md-0 order-1 text-center text-md-start">
           <span className="introText"> I'm <span className="introName">{props.name}</span>,<br />a <span className="universityName">{props.role}</span></span>
           <p className='introPara'>I am passionate in creating responsive, userfriendly and dynamic websites using HTML, CSS, Bootstrap, Tailwind CSS and JavaScript.</p>
+          </div>
+          <div className='col-12 col-md-5 d-flex flex-column justify-content-center order-md-1 order-0 align-self-center'>
+            <div className="imgContainer">
+              <img src="https://i.postimg.cc/C1YrSdSm/mypic.jpg" alt="" className="bg" />
+            </div>
+          </div>
+        </div>
       </div>
-      <img src="https://i.postimg.cc/k5BTj7MW/me.png" alt="" className="bg" />
     </section>
   )
 }
